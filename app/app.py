086754734b7147219f2d7eb8c5895909f23c0a82
rdubwiley <ryan.wiley@amnetgroup.com>
2017-06-27 19:12:26 -0400

    Moving over to mailgun api instead of underwhelming flask_mail

--- app/app.py
+++ app/app.py
@@ -2,12 +2,14 @@ from flask import Flask
 import os
 from flask_bootstrap import Bootstrap
 from flask_mongoengine import MongoEngine
+from flask_mail import Mail
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app  = Flask(__name__)
 app.config.from_pyfile('config.py')
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+mail = Mail(app)
 
 db = MongoEngine(app)
 
--- app/views.py
+++ app/views.py
@@ -3,17 +3,22 @@ import pandas as pd
 import os
 import time
 import json
-from app.app import app, db
+from app.app import app, db, mail
 from app.user import User, Role
 from app.forms.forms import LoginForm, RegisterForm
 from flask import request, render_template, session, url_for, redirect
 from flask_security import Security, MongoEngineUserDatastore, login_required
 
+def test(msg):
+    print(dir(msg))
+    print(msg.body)
+
 user_datastore = MongoEngineUserDatastore(db, User, Role)
-security = Security(app, user_datastore, login_form = LoginForm)
+security = Security(app, user_datastore)
+security.send_mail_task(test)
 
 # Views
 @app.route('/')
 @login_required
 def home():
-    return render_template('index.jade')
+    return 'you did it'
